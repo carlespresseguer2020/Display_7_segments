f8185e7b885a2048204f01bf500d8ee180a9e1ea
carlespresseguer2020 <59728963+carlespresseguer2020@users.noreply.github.com>
2020-02-28 12:55:46 +0100

    Update Display_de_7_segments-P1.ino

--- Display_de_7_segments-P1/Display_de_7_segments-P1.ino
+++ Display_de_7_segments-P1/Display_de_7_segments-P1.ino
@@ -2,81 +2,233 @@
 
   /**********************************************************************************
 **                                                                               **
-*                                                                               *************************************************************
+**                              Array de 8 LEDs                                  **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+//********** Includes *************************************************************
+//********** Variables ************************************************************
+
+    const int f1=2;
+    const int f2=3;
+    const int f3=4;
+    const int f4=5;
+    const int f5=6;
+    const int f6=7;
+    const int f7=8;
+    const int f8=9;
+    const int f9=10;
+    const int p1=11;
+    const int p2=12;
+    const int p3=13;
+    
+    const int temps=100;
+
+//********** Setup ****************************************************************
 void setup()
-{
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(6, OUTPUT);
-}
+  {  
+    pinMode(f1,OUTPUT);
+    pinMode(f2,OUTPUT);
+    pinMode(f3,OUTPUT);
+    pinMode(f4,OUTPUT);
+    pinMode(f5,OUTPUT);
+    pinMode(f6,OUTPUT);
+    pinMode(f7,OUTPUT);
+    pinMode(f8,OUTPUT);
+    pinMode(f9,OUTPUT);
+    pinMode(p1,OUTPUT);
+    pinMode(p2,OUTPUT);
+    pinMode(p3,OUTPUT);
+  }
 //********** Loop ****************************************************************
 void loop()
-
-{
-  //lletra C
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  delay(temps);
-
-  //lletra A
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, LOW);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(temps);
-
-  //lletra R
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, LOW);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, LOW);
-  digitalWrite(G, HIGH);
-  delay(temps);
-
-  //lletra L
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, LOW);
-  delay(temps);
-
-  //lletra E
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(temps);
-
-  //lletra S
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  delay(temps);
-  
+ {
+ 
+     digitalWrite(f1,HIGH);
+     digitalWrite(f2,LOW);
+     digitalWrite(f3,LOW);
+     digitalWrite(f4,LOW);
+     digitalWrite(f5,LOW);
+     digitalWrite(f6,LOW);
+     digitalWrite(f7,LOW);
+     digitalWrite(f8,LOW);
+     digitalWrite(f9,LOW);
+     digitalWrite(p1,HIGH);
+     
+     delay(temps);
+     
+     digitalWrite(f1,LOW);
+     digitalWrite(f2,HIGH);
+     digitalWrite(p1,HIGH);
+     
+     delay(temps);
+     
+     digitalWrite(f2,LOW);
+     digitalWrite(f3,HIGH);
+     digitalWrite(p1,HIGH);
+    
+     delay(temps);
+     
+     digitalWrite(f3,LOW);
+     digitalWrite(f4,HIGH);
+     digitalWrite(p1,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f4,LOW);
+     digitalWrite(f5,HIGH);
+     digitalWrite(p1,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f5,LOW);
+     digitalWrite(f6,HIGH);
+     digitalWrite(p1,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f6,LOW);
+     digitalWrite(f7,HIGH);
+     digitalWrite(p1,HIGH);
+     
+     delay(temps);
+    
+     digitalWrite(f7,LOW);
+     digitalWrite(f8,HIGH);
+     digitalWrite(p1,HIGH);
+     
+     delay(temps);
+    
+     digitalWrite(f8,LOW);
+     digitalWrite(f9,HIGH);
+     digitalWrite(p1,HIGH);
+    
+      delay(temps);
+      
+     digitalWrite(f1,HIGH);
+     digitalWrite(f2,LOW);
+     digitalWrite(f3,LOW);
+     digitalWrite(f4,LOW);
+     digitalWrite(f5,LOW);
+     digitalWrite(f6,LOW);
+     digitalWrite(f7,LOW);
+     digitalWrite(f8,LOW);
+     digitalWrite(f9,LOW);
+     digitalWrite(p1,LOW);
+     digitalWrite(p2,HIGH);
+     
+     delay(temps);
+     
+     digitalWrite(f1,LOW);
+     digitalWrite(f2,HIGH);
+     digitalWrite(p2,HIGH);
+     
+     delay(temps);
+     
+     digitalWrite(f2,LOW);
+     digitalWrite(f3,HIGH);
+     digitalWrite(p2,HIGH);
+    
+     delay(temps);
+     
+     digitalWrite(f3,LOW);
+     digitalWrite(f4,HIGH);
+     digitalWrite(p2,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f4,LOW);
+     digitalWrite(f5,HIGH);
+     digitalWrite(p2,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f5,LOW);
+     digitalWrite(f6,HIGH);
+     digitalWrite(p2,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f6,LOW);
+     digitalWrite(f7,HIGH);
+     digitalWrite(p2,HIGH);
+     
+     delay(temps);
+    
+     digitalWrite(f7,LOW);
+     digitalWrite(f8,HIGH);
+     digitalWrite(p2,HIGH);
+     
+     delay(temps);
+    
+     digitalWrite(f8,LOW);
+     digitalWrite(f9,HIGH);
+     digitalWrite(p2,HIGH);
+    
+    delay(temps);
+     
+     digitalWrite(f1,HIGH);
+     digitalWrite(f2,LOW);
+     digitalWrite(f3,LOW);
+     digitalWrite(f4,LOW);
+     digitalWrite(f5,LOW);
+     digitalWrite(f6,LOW);
+     digitalWrite(f7,LOW);
+     digitalWrite(f8,LOW);
+     digitalWrite(f9,LOW);
+      digitalWrite(p2,LOW);
+     digitalWrite(p3,HIGH);
+     
+     delay(temps);
+     
+     digitalWrite(f1,LOW);
+     digitalWrite(f2,HIGH);
+     digitalWrite(p3,HIGH);
+     
+     delay(temps);
+     
+     digitalWrite(f2,LOW);
+     digitalWrite(f3,HIGH);
+     digitalWrite(p3,HIGH);
+    
+     delay(temps);
+     
+     digitalWrite(f3,LOW);
+     digitalWrite(f4,HIGH);
+     digitalWrite(p3,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f4,LOW);
+     digitalWrite(f5,HIGH);
+     digitalWrite(p3,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f5,LOW);
+     digitalWrite(f6,HIGH);
+     digitalWrite(p3,HIGH);
+    
+     delay(temps);
+    
+     digitalWrite(f6,LOW);
+     digitalWrite(f7,HIGH);
+     digitalWrite(p3,HIGH);
+     
+     delay(temps);
+    
+     digitalWrite(f7,LOW);
+     digitalWrite(f8,HIGH);
+     digitalWrite(p3,HIGH);
+     
+     delay(temps);
+    
+     digitalWrite(f8,LOW);
+     digitalWrite(f9,HIGH);
+     digitalWrite(p3,HIGH);
+ 
 }
+
+
  
 //********** Funcions *************************************************************
